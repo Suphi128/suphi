eaab1ad1cc4c17f01283807348c2eafc82c5ddb3
Suphi128 <tekbasm886@gmail.com>
2025-04-21 10:27:50 +0300

    update

---
+++ exercises1.py
@@ -0,0 +1,18 @@
+#exercises
+name="SUPHI"
+surname="İSLEK"
+print("Welcome to Hell  {} {} how are you".format(name,surname) )
+
+#1
+user=input("Please write your user name : ")
+password=int(input("Please write your password : "))
+if user=="Suphi" and password==4567 :
+    print("Welcome Suphi ")
+else :
+    print("User name or password is wrong ! ")
+
+
+
+                 
+             
+             
